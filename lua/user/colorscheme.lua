0496f162b15e0c8cdcf35137023f0ce86209e05e
Aimen Hamed <aimenhamed@live.com.au>
2022-07-19 12:21:19 +1000

    transparent with tokyo

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
 -- local colorscheme = "darkplus"
 local colorscheme = "tokyonight"
 
+-- vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transparent_sidebar = "true"
+vim.g.tokyonight_transparent = "true"
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
