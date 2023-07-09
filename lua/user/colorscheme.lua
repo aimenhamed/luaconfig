a9d03cab220ba5a410160cf9f6f3825c7101abe5
Aimen Hamed <aimenhamed@live.com.au>
2023-07-09 14:13:20 +1000

    added catppuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,15 +5,23 @@
 --   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
 --   priority = 1000, -- make sure to load this before all the other start plugins
 -- }
-
--- dark plus 
+--
 local M = {
-  "lunarvim/darkplus.nvim",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
 }
 
-M.name = "darkplus"
+-- dark plus 
+-- local M = {
+--   "lunarvim/darkplus.nvim",
+--   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+-- }
+
+-- M.name = "darkplus"
+-- M.name = "tokyonight-night"
+M.name = "catppuccin-mocha"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
